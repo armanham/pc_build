f23616585a60a34c8f91b7f33819c9a2ece87d80
Arman Hambardzumyan <arman.hambardzumyan.2232@gmail.com>
2023-06-09 02:22:32 +0400

    Changes by Arman

--- src/main/java/com/bdg/pc_build/product/model/dto/main_component/CPUCoolerDTO.java
+++ src/main/java/com/bdg/pc_build/product/model/dto/main_component/CPUCoolerDTO.java
@@ -1,11 +1,23 @@
 package com.bdg.pc_build.product.model.dto.main_component;
 
-public record CPUCoolerDTO(
-        String name,
-        Double price,
-        Integer count,
-        Integer fanRPM,
-        String socket,
-        Integer tdp
-) {
+
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.experimental.FieldDefaults;
+
+@Getter
+@Setter
+@FieldDefaults(level = AccessLevel.PRIVATE)
+public class CPUCoolerDTO{
+
+        String name;
+        Double price;
+        Integer count;
+        Integer fanRPM;
+        String socket;
+        Integer tdp;
+
+
+
 }
