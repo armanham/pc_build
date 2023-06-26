5b23239a5729f2f00756e7815f8400fc522a3d1e
ani <ahakobyanani@gmail.com>
2023-06-26 22:23:12 +0400

    Added some exceptions.

---
+++ src/main/java/com/bdg/pc_build/exception/InvalidAuthHeaderException.java
@@ -0,0 +1,11 @@
+package com.bdg.pc_build.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.OK)
+public class InvalidAuthHeaderException extends RuntimeException{
+    public InvalidAuthHeaderException() {
+        super("Invalid authentication header: ");
+    }
+}
