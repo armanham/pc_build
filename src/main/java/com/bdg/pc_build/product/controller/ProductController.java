0c8ae495e79dbbcef762dd2e158f94e11d800e1a
Arman Hambardzumyan <arman.hambardzumyan.2232@gmail.com>
2023-07-16 21:49:55 +0400

    Product list permitted all.

--- src/main/java/com/bdg/pc_build/product/controller/ProductController.java
+++ src/main/java/com/bdg/pc_build/product/controller/ProductController.java
@@ -172,91 +172,76 @@ public class ProductController {
     }
 
     @GetMapping("/case")
-    @PreAuthorize("permitAll()")
     public ResponseEntity<List<CaseDTO>> getAllCases() {
         return ResponseEntity.ok().body(productService.getAllCases());
     }
 
     @GetMapping("/cooler")
-    @PreAuthorize("permitAll()")
     public ResponseEntity<List<CoolerDTO>> getAllCoolers() {
         return ResponseEntity.ok().body(productService.getAllCoolers());
     }
 
     @GetMapping("/cpu-cooler")
-    @PreAuthorize("permitAll()")
     public ResponseEntity<List<CPUCoolerDTO>> getAllCpuCoolers() {
         return ResponseEntity.ok().body(productService.getAllCpuCoolers());
     }
 
     @GetMapping("/cpu")
-    @PreAuthorize("permitAll()") //TODO
     public ResponseEntity<List<CPUDTO>> getAllCpus() {
         return ResponseEntity.ok().body(productService.getAllCpus());
     }
 
     @GetMapping("/gpu")
-    @PreAuthorize("permitAll()")
     public ResponseEntity<List<GPUDTO>> getAllGpus() {
         return ResponseEntity.ok().body(productService.getAllGpus());
     }
 
     @GetMapping("/internal-hard-drive")
-    @PreAuthorize("permitAll()")
     public ResponseEntity<List<InternalHardDriveDTO>> getAllInternalHardDrives() {
         return ResponseEntity.ok().body(productService.getAllInternalHardDrives());
     }
 
     @GetMapping("/motherboard")
-    @PreAuthorize("permitAll()")
     public ResponseEntity<List<MotherboardDTO>> getAllMotherboards() {
         return ResponseEntity.ok().body(productService.getAllMotherboards());
     }
 
     @GetMapping("/power-supply")
-    @PreAuthorize("permitAll()")
     public ResponseEntity<List<PowerSupplyDTO>> getAllPowerSupplies() {
         return ResponseEntity.ok().body(productService.getAllPowerSupplies());
     }
 
     @GetMapping("/ram")
-    @PreAuthorize("permitAll()")
     public ResponseEntity<List<RAMDTO>> getAllRams() {
         return ResponseEntity.ok().body(productService.getAllRams());
     }
 
     @GetMapping("/external-hard-drive")
-    @PreAuthorize("permitAll()")
     public ResponseEntity<List<ExternalHardDriveDTO>> getAllExternalHardDrives() {
         return ResponseEntity.ok().body(productService.getAllExternalHardDrives());
     }
 
     @GetMapping("/headset")
-    @PreAuthorize("permitAll()")
     public ResponseEntity<List<HeadsetDTO>> getAllHeadsets() {
         return ResponseEntity.ok().body(productService.getAllHeadsets());
     }
 
     @GetMapping("/keyboard")
-    @PreAuthorize("permitAll()")
     public ResponseEntity<List<KeyboardDTO>> getAllKeyboards() {
         return ResponseEntity.ok().body(productService.getAllKeyboards());
     }
 
     @GetMapping("/monitor")
-    @PreAuthorize("permitAll()")
     public ResponseEntity<List<MonitorDTO>> getAllMonitors() {
         return ResponseEntity.ok().body(productService.getAllMonitors());
     }
 
     @GetMapping("/mouse")
-    @PreAuthorize("permitAll()")
     public ResponseEntity<List<MouseDTO>> getAllMice() {
         return ResponseEntity.ok().body(productService.getAllMice());
     }
 
     @GetMapping("/speaker")
-    @PreAuthorize("permitAll()")
     public ResponseEntity<List<SpeakerDTO>> getAllSpeakers() {
         return ResponseEntity.ok().body(productService.getAllSpeakers());
     }
